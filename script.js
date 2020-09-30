0aa871f334dea046d889efcb5e64a6cbd2dfc904
Madalynn Sandoval <sandovalmdalynn@gmail.com>
2020-09-30 16:55:30 -0600

    Made color picker.

--- script.js
+++ script.js
@@ -7,37 +7,34 @@ function initColorPicker() {
     let rgb = {
         red: document.getElementById("red"),
         green: document.getElementById("green"),
-        blue: {
-            value: 255
-        }
+        blue: document.getElementById("blue"),
     };
-    let colorPickers = document.getElementsByClassName(picker);
+    let colorPickers = document.getElementsByClassName("picker");
     setColorPickerEventListeners(colorBox, rgb, colorPickers);
 }
 
 function setColorPickerEventListeners(element, colors, pickerElements) {
     let pickerLen = pickerElements.length;
-    for (let i = 0; i < pickerLen; i++) {
-        pickerElements[i].addEventListener('change,' () => {
-            console.log("Red value:", rgb.red.value);
+    for (let i =  0; i < pickerLen; i++) {
+        pickerElements[i].addEventListener('change', () => {
             let red= colors.red.value;
             let green = colors.green.value;
             let blue = colors.blue.value;
-            setBoxBGColor(element, red, green, blue);
+            setElementBGColor(element, red, green, blue);
             setDisplayValues(red, green, blue)
         });
     }
 }
 
-
-function setElementBGColor (element, red, green, blue) {
+function setElementBGColor(bgElement, red, green, blue) {
     let rgbVal = [red, green, blue].join(',');
-    colorBox.style.backgroundColor + "rgb(" + rgbVal + ")"
+     bgElement.style.backgroundColor = "rgb(" + rgbVal + ")";
 }
-function setDisplayValues(red, gree, blue) {
-    let redVal = document.getElementById("redVal")
-    let redVal = document.getElementById("greenVal")
-    let redVal = document.getElementById("blueVal")
+
+function setDisplayValues(red, green, blue) {
+    let redVal = document.getElementById("redVal");
+    let greenVal = document.getElementById("greenVal");
+    let blueVal = document.getElementById("blueVal")
 
     redVal.innerText = red;
     greenVal.innerText = green;
